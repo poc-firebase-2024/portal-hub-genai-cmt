eb22d6b4ba0a25938b8779f0a1f2899e861ace5d
portal-hub-genai-cmt <poc.firebase.2024@gmail.com>
2024-09-04 22:51:49 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World!
+          Hello World! React App!
         </a>
       </header>
     </div>
